91ba52ea32b7c37fe6637caf0d9a599c5200c270
胡贵 <254963746@qq.com>
2015-02-06 10:44:05 +0800

    performance something

--- job-common/src/main/java/com/lts/job/common/cluster/NodeManager.java
+++ job-common/src/main/java/com/lts/job/common/cluster/NodeManager.java
@@ -1,6 +1,7 @@
 package com.lts.job.common.cluster;
 
 
+import com.lts.job.common.support.Application;
 import com.lts.job.common.util.CollectionUtils;
 import com.lts.job.common.util.ListUtils;
 import org.slf4j.Logger;
@@ -12,7 +13,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * @author Robert HG (254963746@qq.com) on 6/22/14.
- * 节点管理 (主要用于管理自己关注的节点)
+ *         节点管理 (主要用于管理自己关注的节点)
  */
 public class NodeManager {
 
@@ -20,13 +21,21 @@ public class NodeManager {
     private static final ConcurrentHashMap<NodeType, List<Node>> NODES = new ConcurrentHashMap<NodeType, List<Node>>();
 
     public static void addNode(Node node) {
-        List<Node> nodeList = NODES.get(node.getNodeType());
-        if (CollectionUtils.isEmpty(nodeList)) {
-            nodeList = new CopyOnWriteArrayList<Node>();
-            NODES.put(node.getNodeType(), nodeList);
+        // JobClient 和 TaskTracker 只对自己同一个组的节点关注
+        // JobTracker 要对所有节点都要关注
+        if ((Application.Config.getNodeType().equals(node.getNodeType())
+                && Application.Config.getNodeGroup().equals(node.getGroup()))
+                || (NodeType.JOB_TRACKER.equals(Application.Config.getNodeType()))
+                ) {
+
+            List<Node> nodeList = NODES.get(node.getNodeType());
+            if (CollectionUtils.isEmpty(nodeList)) {
+                nodeList = new CopyOnWriteArrayList<Node>();
+                NODES.put(node.getNodeType(), nodeList);
+            }
+            nodeList.add(node);
+            LOGGER.info("添加节点" + node);
         }
-        nodeList.add(node);
-        LOGGER.info("添加节点" + node);
     }
 
     public static List<Node> getNodeList(NodeType nodeType, final String nodeGroup) {
@@ -40,13 +49,14 @@ public class NodeManager {
             }
         });
     }
+
     public static List<Node> getNodeList(NodeType nodeType) {
         return NODES.get(nodeType);
     }
 
     public static void removeNode(Node node) {
         List<Node> nodeList = NODES.get(node.getNodeType());
-        if(nodeList.remove(node)){
+        if (nodeList.remove(node)) {
             LOGGER.info("删除节点" + node);
         }
     }
