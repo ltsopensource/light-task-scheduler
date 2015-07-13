3e71dc176c37d758b2bd3fd6b13cd6cced2045f4
胡贵 <hugui@raycloud.com>
2015-07-14 06:56:21 +0800

    sth

--- lts-example/src/main/java/com/lts/example/api/JobTrackerTest.java
+++ lts-example/src/main/java/com/lts/example/api/JobTrackerTest.java
@@ -61,7 +61,7 @@ public class JobTrackerTest {
         final JobTracker jobTracker = new JobTracker();
         // 节点信息配置
         jobTracker.setRegistryAddress("zookeeper://127.0.0.1:2181");
-        jobTracker.setRegistryAddress("redis://127.0.0.1:6379");
+//        jobTracker.setRegistryAddress("redis://127.0.0.1:6379");
         jobTracker.setListenPort(35002); // 默认 35001
         jobTracker.setClusterName("test_cluster");
 
