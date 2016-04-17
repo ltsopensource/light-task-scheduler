258d01ace12d5e322a36e87bf0510f7504e3f48b
hugui <254963746@qq.com>
2016-04-17 15:24:24 +0800

    去掉 javassist

--- lts-core/src/main/java/com/github/ltsopensource/core/compiler/AbstractCompiler.java
+++ lts-core/src/main/java/com/github/ltsopensource/core/compiler/AbstractCompiler.java
@@ -49,42 +49,5 @@ public abstract class AbstractCompiler implements Compiler {
     }
 
     protected abstract Class<?> doCompile(String name, String source) throws Throwable;
-
-    public static void main(String[] args) {
-        String code = "package com.github.ltsopensource.core.support.bean;\n" +
-                "import com.github.ltsopensource.core.support.bean.BeanCopier;\n" +
-                "import com.github.ltsopensource.queue.domain.JobPo;\n" +
-                "public class JobPo2JobPoBeanCopier1 implements BeanCopier<JobPo,JobPo> {\n" +
-                "public void copyProps(JobPo source, JobPo target){\n" +
-                "target.setJobId(source.getJobId());\n" +
-                "target.setJobType(source.getJobType());\n" +
-                "target.setPriority(source.getPriority());\n" +
-                "target.setTaskId(source.getTaskId());\n" +
-                "target.setRealTaskId(source.getRealTaskId());\n" +
-                "target.setGmtCreated(source.getGmtCreated());\n" +
-                "target.setGmtModified(source.getGmtModified());\n" +
-                "target.setSubmitNodeGroup(source.getSubmitNodeGroup());\n" +
-                "target.setTaskTrackerNodeGroup(source.getTaskTrackerNodeGroup());\n" +
-                "com.github.ltsopensource.core.support.bean.PropConverter<JobPo, java.util.Map<java.lang.String, java.lang.String>> extParamsConverter = (com.github.ltsopensource.core.support.bean.PropConverter<JobPo, java.util.Map<java.lang.String, java.lang.String>> )com.github.ltsopensource.core.support.bean.BeanCopierFactory.getConverter(1,\"extParams\");\n" +
-                "target.setExtParams((java.util.Map<java.lang.String, java.lang.String>)extParamsConverter.convert(source));\n" +
-                "com.github.ltsopensource.core.support.bean.PropConverter<JobPo, java.util.Map<java.lang.String, java.lang.String>> internalExtParamsConverter = (com.github.ltsopensource.core.support.bean.PropConverter<JobPo, java.util.Map<java.lang.String, java.lang.String>> )com.github.ltsopensource.core.support.bean.BeanCopierFactory.getConverter(1,\"internalExtParams\");\n" +
-                "target.setInternalExtParams((java.util.Map<java.lang.String, java.lang.String>)internalExtParamsConverter.convert(source));\n" +
-                "target.setTaskTrackerIdentity(source.getTaskTrackerIdentity());\n" +
-                "target.setNeedFeedback(source.isNeedFeedback());\n" +
-                "target.setCronExpression(source.getCronExpression());\n" +
-                "target.setTriggerTime(source.getTriggerTime());\n" +
-                "target.setRetryTimes(source.getRetryTimes());\n" +
-                "target.setMaxRetryTimes(source.getMaxRetryTimes());\n" +
-                "target.setRepeatCount(source.getRepeatCount());\n" +
-                "target.setRepeatedCount(source.getRepeatedCount());\n" +
-                "target.setRepeatInterval(source.getRepeatInterval());\n" +
-                "target.setRelyOnPrevCycle(source.getRelyOnPrevCycle());\n" +
-                "target.setLastGenerateTriggerTime(source.getLastGenerateTriggerTime());\n" +
-                "}}";
-
-        Compiler compiler = new JavassistCompiler();
-        compiler.compile(code);
-
-    }
 }
 
