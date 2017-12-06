db6d4447194cf027051052a38cd0babb38ba4813
Kervin <zy5211314521@outlook.com>
2017-12-06 11:52:19 +0800

    样例

---
+++ lts-examples/lts-example-tasktracker/lts-example-tasktracker-tester/src/main/java/com/github/ltsopensource/example/TestJobRunner.java
@@ -0,0 +1,34 @@
+package com.github.ltsopensource.example;
+
+import com.github.ltsopensource.core.domain.Action;
+import com.github.ltsopensource.core.logger.Logger;
+import com.github.ltsopensource.core.logger.LoggerFactory;
+import com.github.ltsopensource.tasktracker.Result;
+import com.github.ltsopensource.tasktracker.logger.BizLogger;
+import com.github.ltsopensource.tasktracker.runner.JobContext;
+import com.github.ltsopensource.tasktracker.runner.JobRunner;
+
+/**
+ * @author Robert HG (254963746@qq.com) on 8/19/14.
+ */
+public class TestJobRunner implements JobRunner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestJobRunner.class);
+
+    @Override
+    public Result run(JobContext jobContext) throws Throwable {
+        try {
+            BizLogger bizLogger = jobContext.getBizLogger();
+
+            // TODO 业务逻辑
+            LOGGER.info("我要执行：" + jobContext);
+            // 会发送到 LTS (JobTracker上)
+            bizLogger.info("测试，业务日志啊啊啊啊啊");
+
+        } catch (Exception e) {
+            LOGGER.info("Run job failed!", e);
+            return new Result(Action.EXECUTE_FAILED, e.getMessage());
+        }
+        return new Result(Action.EXECUTE_SUCCESS, "执行成功了，哈哈");
+    }
+}
