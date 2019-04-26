c0e8337e9a32318711a81dfe90f529a74bf2b7d2
chenwenshun <wenshun.chen@freemud.cn>
2019-04-26 11:22:59 +0800

    对与返回参数不是Result类型的方法避免执行不到与异常

--- lts-spring/src/main/java/com/github/ltsopensource/spring/tasktracker/JobRunnerBuilder.java
+++ lts-spring/src/main/java/com/github/ltsopensource/spring/tasktracker/JobRunnerBuilder.java
@@ -18,25 +18,32 @@ public class JobRunnerBuilder {
         return new JobRunner() {
             @Override
             public Result run(JobContext jobContext) throws Throwable {
+                Object resultObj;
                 if (pTypes == null || pTypes.length == 0) {
-                    return (Result) targetMethod.invoke(targetObject);
-                }
-                Object[] pTypeValues = new Object[pTypes.length];
+                    resultObj = targetMethod.invoke(targetObject);
+                }else{
 
-                for (int i = 0; i < pTypes.length; i++) {
-                    if (pTypes[i] == Job.class) {
-                        pTypeValues[i] = jobContext.getJob();
-                    } else if (pTypes[i] == JobContext.class) {
-                        pTypeValues[i] = jobContext;
-                    } else {
-                        pTypeValues[i] = null;
+                    Object[] pTypeValues = new Object[pTypes.length];
+
+                    for (int i = 0; i < pTypes.length; i++) {
+                        if (pTypes[i] == Job.class) {
+                            pTypeValues[i] = jobContext.getJob();
+                        } else if (pTypes[i] == JobContext.class) {
+                            pTypeValues[i] = jobContext;
+                        } else {
+                            pTypeValues[i] = null;
+                        }
                     }
+
+                    resultObj = targetMethod.invoke(targetObject, pTypeValues);
                 }
+
                 Class<?> returnType = targetMethod.getReturnType();
                 if (returnType != Result.class) {
                     return new Result(Action.EXECUTE_SUCCESS);
                 }
-                return (Result) targetMethod.invoke(targetObject, pTypeValues);
+
+                return (Result) resultObj;
             }
         };
     }
